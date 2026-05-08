fa21478032387af8e8c65462f58b267cd3051903
rishita-pixie <nainwalrishita87@gmail.com>
2026-05-08 22:14:06 +0530

    ques 1 week 6

--- cpp_ques/week6-1.cpp
+++ cpp_ques/week6-1.cpp
@@ -2,10 +2,7 @@
 #include<vector>
 using namespace std;
 
-vector<int> adj[100];
-int vis[100];
-
-void dfs(int node)
+void dfs(int node, vector<vector<int>>& adj, vector<int>& vis)
 {
     vis[node]=1;
 
@@ -13,7 +10,7 @@ void dfs(int node)
     {
         if(!vis[x])
         {
-            dfs(x);
+            dfs(x,adj,vis);
         }
     }
 }
@@ -23,6 +20,8 @@ int main()
     int n,e;
     cin>>n>>e;
 
+    vector<vector<int>> adj(n);
+
     for(int i=0;i<e;i++)
     {
         int u,v;
@@ -31,10 +30,13 @@ int main()
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
     int src,dest;
     cin>>src>>dest;
 
-    dfs(src);
+    vector<int> vis(n,0);
+
+    dfs(src,adj,vis);
 
     if(vis[dest])
         cout<<"Yes Path Exists";
